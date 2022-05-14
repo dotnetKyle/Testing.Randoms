3501e1d9b1785952c56a71f235a0794728292d29
Kyle Burrows <dotnetKyle@gmail.com>
2022-05-14 13:31:58 -0400

    initial commit

---
+++ src/Testing.Randoms/RandomGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Testing.Randoms;
+
+public static class RandomGenerator
+{
+    static RandomNumberGenerator _randomNumberGenerator = RandomNumberGenerator.Create();
+
+    public static string GetRandomString(int length = 12, bool removeSpecialChars = true)
+    {
+        var bytes = new byte[length];
+        _randomNumberGenerator.GetBytes(bytes);
+        var base64 = Convert.ToBase64String(bytes);
+        if (removeSpecialChars)
+        {
+            return base64
+                .Replace('/', 'x')
+                .Replace('+', 'x')
+                .Replace('=', 'x');
+        }
+        else
+        {
+            return base64;
+        }
+    }
+}
